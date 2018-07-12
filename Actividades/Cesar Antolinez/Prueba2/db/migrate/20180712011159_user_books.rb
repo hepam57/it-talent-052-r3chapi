f6630f9312fb534cd8f05ab630ed2d3bf097191b
CesarAntoliez <cesaraleajndroantolinez@gmail.com>
2018-07-11 21:09:14 -0500

    Pruebas

---
+++ Actividades/Cesar Antolinez/Prueba2/db/migrate/20180712011159_user_books.rb
@@ -0,0 +1,9 @@
+class UserBooks < ActiveRecord::Migration[5.0]
+  def change
+  	create_table :user_books do |t|
+      t.belongs_to :user
+      t.belongs_to :book
+      t.timestamps
+    end
+  end
+end
