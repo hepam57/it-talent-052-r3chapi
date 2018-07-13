f653aea40383c25653009c143fdf8afe59a1437d
CesarAntoliez <cesaraleajndroantolinez@gmail.com>
2018-07-12 21:38:37 -0500

    Crear angular 2 y generar http

---
+++ Actividades/Cesar Antolinez/angular2/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MoviesService } from './services/movies.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string;
+  mostrar: boolean;
+  arreglo: Array<number>;
+  captar: any;
+  public movie: any;
+  constructor(private movioService: MoviesService) {
+    this.mostrar = false;
+    this.arreglo = [0, 1, 2, 3, 4, 5];
+  }
+  public agregar(): void {
+    this.arreglo.push(this.captar);
+  }
+  public searchMovie(): void {
+    this.movioService.getMovieByTitle(this.title)
+      .subscribe( (response: any) => {
+        this.movie = response;
+        console.log(response);
+      });
+  }
+
+}
