5504ca877902fc8df37211ce29d9e04219b693e4
c0der97 <blae1997@gmail.com>
2018-07-17 19:48:29 -0500

    tercera aplicacion angular

---
+++ Actividades/Angel leon/angular/tercera-app/src/app/componente-uno/componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'app-movie',
+    templateUrl: './componente.component.html'
+})
+
+export class MovieComponent{
+    constructor(){
+
+    }
+}
