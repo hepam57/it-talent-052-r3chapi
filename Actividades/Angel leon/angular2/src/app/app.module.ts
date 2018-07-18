6bcb1df386f754e13bfd0b142e83a112a0d7c8c1
c0der97 <blae1997@gmail.com>
2018-07-17 20:02:13 -0500

    app consumo service

---
+++ Actividades/Angel leon/angular2/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {MoviesService} from './services/movies.service';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [MoviesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
