a464beb2b0b6a6345896253fc717ed3714e28360
c0der97 <blae1997@gmail.com>
2018-07-19 19:14:12 -0500

    primera app ionic

--- Actividades/Angel leon/primera-app/src/pages/home/home.ts
+++ Actividades/Angel leon/primera-app/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+    public platform: Platform,
+    public geolocation: Geolocation) {
+      this.platform.ready().then(()=>{
+        this.geolocation.getCurrentPosition().then((position)=>{
+          console.log(position);
+        });
+      })
   }
 
 }
