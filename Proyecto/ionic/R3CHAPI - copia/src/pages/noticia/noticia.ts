f4855279a1804005b1457b4093c78968f0b9d458
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2018-07-26 21:30:40 -0500

    Cambios apk

--- Proyecto/ionic/R3CHAPI - copia/src/pages/noticia/noticia.ts
+++ Proyecto/ionic/R3CHAPI - copia/src/pages/noticia/noticia.ts
@@ -19,8 +19,10 @@ export class NoticiaPage {
     this.noticia = {
       'id':'3',
       'titulo':'Noticia 3',
-      'description':'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500.',
+      'description':'<h1>Lorem</h1> <b>Ipsu</b> es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500.',
+      'url':'http://static.t13.cl/images/sizes/1200x675/1532532005-rick-and-morty-658x370-4b072da7af5b7106.jpg'
     };
+    console.log(this.noticia);
   }
 
   ionViewDidLoad() {
